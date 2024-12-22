39f410e5e06f2176a0eed09bc4a9d7c16ec50f3d
Yash Diwaker <69832240+NoobCoderY@users.noreply.github.com>
2024-12-22 22:37:03 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ const app = express();
 
  app.use(cors({
     credentials: true,
-    origin:["http://localhost:3000","https://magnet-brains-fe.vercel.app/"],
+    origin:["https://magnet-brains-fe.vercel.app"],
   }))
   app.use(express.json());
   app.use(express.urlencoded({
